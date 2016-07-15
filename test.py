de68c640820d4275d425a6f4f16a660358a695e4
HanVincent <b84414@gmail.com>
2016-07-14 21:12:07 -0700

    Lowercase and Stemming and Filtering stopwords.

--- test.py
+++ test.py
@@ -4,39 +4,43 @@ import scipy
 import random
 import collections
 from nltk.stem.porter import *
+from nltk.corpus import stopwords
 
 print("Reading data...")
 data = list(open("test.tsv"))
 print("done!")
 
-#read the dictionary
-dictionary = collections.defaultdict(int)
-f = open("dict.tsv", 'r')
+###read the dictionary and store in dict
 
-for eachline in f:
+dictionary = collections.defaultdict(int)
+fr = open("dict.tsv", 'r')
+for eachline in fr:
     a = eachline.split(',')
     dictionary[a[0]] = a[1]
-
-f.close()
+fr.close()
     
-#start to calcu
+###start to calculate each phrase
+    
 stemmer = PorterStemmer()
 fw = open("predict.csv", 'w')
 fw.write("PhraseId,Sentiment\n")
 for each in data[1:]:
-    split = each.split('\t')
-    phrase = split[2]
-    totalScore = 2
-    for each_token in phrase.lower().split():
+    totalScore = 0
+    splitLine = each.split('\t')
+    #phrase = splitLine[2].split()
+    phrase = splitLine[2].lower().split()
+    phrase = [word for word in phrase if word not in stopwords.words('english')]
+    for each_token in phrase:
+        #stem_token = each_token        
         stem_token = stemmer.stem(each_token)
         if stem_token in dictionary:      
             totalScore += int(dictionary[stem_token])
         else:
             totalScore += 2
-    
-    totalScore = 2 if len(phrase.split()) == 0 else totalScore / len(phrase.split())
+            
+    totalScore = 2 if len(phrase) == 0 else totalScore / len(phrase)
         
-    fw.write(split[0] + "," + str(round(totalScore)) + "\n")
+    fw.write(splitLine[0] + "," + str(round(totalScore)) + "\n")
     
 fw.close()
     
--- train.py
+++ train.py
@@ -5,37 +5,51 @@ import nltk
 import random
 import collections
 from nltk.stem.porter import *
+from nltk.corpus import stopwords
 
-#
+### function for calculate the most common sentiment
+
 def getFrequent(value):#value:[1,4,2,2,5,3,2]
-    dict_count = collections.defaultdict(int)    
+    dict_count = collections.defaultdict(int)#[1,2,3,4,5] 
     for v in value:
         dict_count[v] += 1
-    common = max(dict_count, key = dict_count.get)#未排除相等數量
+    common = max(dict_count, key = dict_count.get)#未排除相等數量, key=value.get return key
     return common
         
-
 print("Reading data...")
 data = list(open("train.tsv", "r"))
 print("done!")
 
-dict_statistics = collections.defaultdict(list)#record each word and its sentiment
+###record each word and its sentiment
 
-#grab each word's sentiment and lowercase
+dict_statistics = collections.defaultdict(list)
+
+###grab each word's sentiment, stemming and lowercase
+
 stemmer = PorterStemmer()
 for each in data[1:]:
-    phrase = each.split('\t')[2].lower()
-    for each_token in phrase.split():
-        dict_statistics[stemmer.stem(each_token)].append(each.split('\t')[3].strip())
+    splitLine = each.split('\t')
+    #phrase = splitLine[2].split()
+    phrase = splitLine[2].lower().split()
+    phrase = [word for word in phrase if word not in stopwords.words('english')]
+    for each_token in phrase:
+        stemmed_word = stemmer.stem(each_token)
+        dict_statistics[stemmed_word].append(splitLine[3].strip())
 
-dict_token = collections.defaultdict(int)#real dictionary for sentiment
+###real dictionary for sentiment
+dict_token = collections.defaultdict(int)
+
+###use function to calcu number and establish the real dictionary.
 
 for key, value in dict_statistics.items():
     common = getFrequent(value)
-    dict_token[key] = common#establish the real dictionary.
+    dict_token[key] = common
+    
+###write down the dictionary
     
 f = open("dict.tsv", 'w')
 for key, value in dict_token.items():
     f.write(key+","+value+"\n")
 f.close()
-#get the dictionary for each word's sentiment
+
+###get the dictionary for each word's sentiment
