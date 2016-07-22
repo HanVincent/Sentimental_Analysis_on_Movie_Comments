c6b3c5abf72c6a6f80d6dfb1137214d32f93ed3e
HanVincent <b84414@gmail.com>
2016-07-22 00:55:42 -0700

    Beautify the code and Add n-gram code.

---
+++ process.py
@@ -0,0 +1,116 @@
+from collections import defaultdict
+import string
+from nltk.corpus import stopwords
+from nltk.stem.porter import *
+
+def processData(dataset, select):
+  temp_data = []
+  ### How many unique words are there?
+  if select == "1":
+    for datum in dataset:
+      temp_data.append(datum)
+    print("Do nothing.")
+
+  ### Ignore capitalization and remove punctuation
+  elif select == "2":
+    punctuation = set(string.punctuation)
+    for datum in dataset:
+      line = ''.join([c for c in datum.lower() if not c in punctuation])
+      temp_data.append(line)
+    print("Ignore capitalization and remove punctuation")
+
+  ### With stemming
+  elif select == "3":
+    punctuation = set(string.punctuation)
+    stemmer = PorterStemmer()
+    for datum in dataset:
+      line = ''.join([c for c in datum.lower() if not c in punctuation])  
+      line = ' '.join([stemmer.stem(w) for w in line.split()])
+      temp_data.append(line)
+    print("With stemming")
+
+  ### Remove Stopwords
+  elif select == "4":
+    punctuation = set(string.punctuation)
+    stemmer = PorterStemmer()
+    for datum in dataset:
+      line = ''.join([c for c in datum.lower() if not c in punctuation]) 
+      r_words = [word for word in line.split() if word not in stopwords.words('english')]
+      line = ' '.join([stemmer.stem(w) for w in r_words])
+      temp_data.append(line)    
+    print("Remove stopwords")
+
+  return temp_data
+
+###########################################################################
+  
+def countWord(dataset, select):
+  wordCount = defaultdict(int)
+  
+  ### Unigram
+  if select == "1":
+    for datum in dataset:
+      for w in datum.split():
+        wordCount[w] += 1
+    print("Unigram: ", len(wordCount))
+
+  ### Bigram
+  elif select == "2":
+    for datum in dataset:
+      words = datum.split()
+      for index in range(len(words)-1):
+        w = ' '.join([words[index], words[index+1]])
+        wordCount[w] += 1
+    print("Bigram: ", len(wordCount))
+
+  ### Trigram
+  elif select == "3":
+    for datum in dataset:
+      words = datum.split()
+      for index in range(len(words)-2):
+        w = ' '.join([words[index], words[index+1], words[index+2]])
+        wordCount[w] += 1
+    print("Trigram: ", len(wordCount))
+
+  ### Unigram + Bigram
+  elif select == "4":
+    for datum in dataset:
+      words = datum.split()
+      for index in range(len(words)-1):
+        w = ' '.join([words[index], words[index+1]])
+        wordCount[w] += 1 #bigram
+        wordCount[words[index]] #unigram
+      if len(words) > 0:
+        wordCount[words[len(words)-1]] += 1  #unigram - last word
+    print("Unigram + Bigram: ", len(wordCount))
+
+  ### Unigram + Bigram + Trigram    
+  elif select == "5":
+    for datum in dataset:
+      words = datum.split()
+      for index in range(len(words)-2):
+        w = ' '.join([words[index], words[index+1], words[index+2]])
+        wordCount[w] += 1 #trigram
+        w = ' '.join([words[index], words[index+1]])
+        wordCount[w] += 1 #bigram
+        wordCount[words[index]] += 1 #unigram
+        
+      if len(words) > 1: #bigram - last pair
+        w = ' '.join([words[len(words)-2], words[len(words)-1]]) 
+        wordCount[w] += 1 
+        
+      if len(words) > 0: #unigram - last two words
+        wordCount[words[len(words)-2]] += 1  
+        wordCount[words[len(words)-1]] += 1
+      
+  return wordCount
+
+###########################################################################
+  
+
+"""
+if __name__ == '__main__':
+    # test1.py executed as script
+    # do something
+    processData()
+    """
--- regression.py
+++ regression.py
@@ -4,6 +4,7 @@ Try to use Regression to classify the sentimental analysis.
 y = x * theta
 """
 import numpy
+from process import *
 from collections import defaultdict
 import string
 from nltk.stem.porter import *
@@ -11,67 +12,29 @@ from nltk.corpus import stopwords
 from sklearn import linear_model
 
 ### Read the training data
-
 print("Reading data...")
-data = list(open("train.tsv", "r"))
+data = list(open("train.tsv", "r"))[1:]
 print("done")
 
-### Process the data and the data structure
-
-p_data = defaultdict(int)
-for d in data[1:]:
+### Process the data structure
+p_data = []
+p_data_v = []
+for d in data:
     col = d.split('\t')
-    p_data[col[2]] = float(col[3].strip())
-
-print("1.Original")
-print("2.Lowercase and remove punct.")
-print("3.plus stemming")
-print("4.remove stopwords")
+    p_data.append(col[2])
+    p_data_v.append(float(col[3].strip()))
+    
+### Process
+print("1.Original\n2.Lowercase and remove punct.\n3.plus stemming\n4.remove stopwords")
 select = input("Select: ")
+p_data = processData(p_data, select)
 
-wordCount = defaultdict(int)
+### Gram
+print("1.Unigram\n2.Bigram\n3.Trigram\n4.Unigram + Bigram\n5.Unigram + Bigram + Trigram\n")
+gram_select = input("Select n-gram: ")
+wordCount = countWord(p_data, gram_select)
 
-### How many unique words are there?
-if select == "1":
-  for k, v in p_data.items():
-    for w in k.split():
-      wordCount[w] += 1
-  print("lenth of original wordCount:", len(wordCount))
-
-### Ignore capitalization and remove punctuation
-elif select == "2":
-  punctuation = set(string.punctuation)
-  for k, v in p_data.items():
-    r = ''.join([c for c in k.lower() if not c in punctuation])
-    for w in r.split():
-      wordCount[w] += 1
-  print("length of punctuation: ", len(wordCount))
-
-### With stemming
-elif select == "3":
-  punctuation = set(string.punctuation)
-  stemmer = PorterStemmer()
-  for k, v in p_data.items():
-    r = ''.join([c for c in k.lower() if not c in punctuation])  
-    for w in r.split():
-      w = stemmer.stem(w)
-      wordCount[w] += 1
-  print("length of stem: ", len(wordCount))
-
-### Remove Stopwords
-elif select == "4":
-  punctuation = set(string.punctuation)
-  stemmer = PorterStemmer()
-  for k, v in p_data.items():
-    r = ''.join([c for c in k.lower() if not c in punctuation]) 
-    r_words = [word for word in r.split() if word not in stopwords.words('english')]
-    for w in r_words:
-      w = stemmer.stem(w)
-      wordCount[w] += 1
-  print("length of removing stopwords: ", len(wordCount))
-
-
-### Just take 1000 words to train
+### Sort in order
 counts = [(wordCount[w], w) for w in wordCount]
 counts.sort()
 counts.reverse()
@@ -83,47 +46,76 @@ print(words)
 #======================================================================#
 
 ### Sentiment analysis
-wordId = dict(zip(words, range(len(words))))#會依照字母出現頻率做index, a:0, the:1...
-wordSet = set(words)
+wordId = dict(zip(words, range(len(words))))# a:0, the:1...
 
-def feature(datum, select):
+def feature(datum, gram_select):
   feat = [0] * len(words)
-    
-  ### Original  
-  if select == "1":
+
+  ### Unigram
+  if gram_select == "1":
     for w in datum.split():
       if w in words:
         feat[wordId[w]] += 1
-  
-  ### Lowercase and remove punctuation  
-  elif select == "2":
-    r = ''.join([c for c in datum.lower() if not c in punctuation])
-    for w in r.split():
+        
+  ### Bigram
+  if gram_select == "2":
+    w_split = datum.split()    
+    for index in range(len(w_split)-1):
+      w = ' '.join([w_split[index], w_split[index+1]])
+      if w in words:
+        feat[wordId[w]] += 1
+        
+  ### Trigram
+  if gram_select == "3":
+    w_split = datum.split()
+    for index in range(len(w_split)-2):
+      w = ' '.join([w_split[index], w_split[index+1], w_split[index+2]])
+      if w in words:
+        feat[wordId[w]] += 1
+      
+  ### Unigram + Bigram
+  if gram_select == "4":
+    w_split = datum.split()
+    for index in range(len(w_split)-1):
+      w = ' '.join([w_split[index], w_split[index+1]])
+      if w in words:
+        feat[wordId[w]] += 1
+      if w_split[index] in words:
+        feat[wordId[w_split[index]]] += 1
+    if len(w_split) > 0 and w_split[len(w_split)-1] in words:
+      feat[wordId[w_split[len(w_split)-1]]] += 1
+      
+  ### Unigram + Bigram + Trigram
+  if gram_select == "5":
+    w_split = datum.split()
+    for index in range(len(w_split)-2):
+      w = ' '.join([w_split[index], w_split[index+1], w_split[index+2]]) #Trigram
+      if w in words: 
+        feat[wordId[w]] += 1
+      w = ' '.join([w_split[index], w_split[index+1]]) #Bigram
+      if w in words: 
+        feat[wordId[w]] += 1
+      w = w_split[index] #Unigram
       if w in words:
         feat[wordId[w]] += 1
     
-  ### Stemming
-  elif select == "3":  
-    r = ''.join([c for c in datum.lower() if not c in punctuation])
-    for w in r.split():
-      w = stemmer.stem(w)
+    if len(w_split) > 1: #Last bigram term
+      w = ' '.join([w_split[len(w_split)-2], w_split[len(w_split)-1]])
       if w in words:
-        feat[wordId[w]] += 1
-    
-  ### Remove stopwords
-  elif select == "4":  
-    r = ''.join([c for c in datum.lower() if not c in punctuation])
-    r_words = [word for word in r.split() if word not in stopwords.words('english')]
-    for w in r_words:
-      w = stemmer.stem(w)
-      if w in words:
-        feat[wordId[w]] += 1
+          feat[wordId[w]] += 1
+
+    if len(w_split) > 0: #Last unigram term
+      if w_split[len(w_split)-2] in words:
+        feat[wordId[w_split[len(w_split)-2]]] += 1 
+      if w_split[len(w_split)-1] in words:
+        feat[wordId[w_split[len(w_split)-1]]] += 1
  
   feat.append(1)     
   return feat
-    
-X = [feature(k, select) for k, v in p_data.items()]#每個instance都會有自己的向量
-y = [v for k, v in p_data.items()]#y = answer(rank)
+  
+### Setting y = X * theta
+X = [feature(datum, gram_select) for datum in p_data]
+y = [v for v in p_data_v]
 
 ### Training
 clf = linear_model.Ridge(1.0, fit_intercept=False)
@@ -135,7 +127,16 @@ print("Reading test data...")
 test_data = list(open("test.tsv", "r"))[1:]
 print("done")
 
-test_X = [feature(d.split('\t')[2], select) for d in test_data]
+p_test_data = []
+for d in test_data:
+    col = d.split('\t')
+    p_test_data.append(col[2])
+
+### Process  
+p_test_data = processData(p_test_data, select)
+  
+### Comparing string
+test_X = [feature(datum, gram_select) for datum in p_test_data]
 
 ### Predict
 predictions = numpy.round(clf.predict(test_X))
